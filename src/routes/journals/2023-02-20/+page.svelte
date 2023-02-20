4ca8d7ee71bec732abe73720ccd5b7c4d514d9cf
Hisam Fahri <hisam.fahri@pitik.id>
2023-02-20 21:05:12 +0700

    feat(journals): add journal `2023-02-20`

---
+++ src/routes/journals/2023-02-20/+page.svelte
@@ -0,0 +1,95 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Personalised Development Environment - Hisam's Journal</title>
+</svelte:head>
+
+<div class="mt-24 mb-12">
+	<h1 class="text-xl font-semibold">Personalised Development Environment</h1>
+	<p class="text-base font-normal text-gray-400">2023-02-20 by <a href="/">Hisam Fahri</a></p>
+</div>
+
+<div class="content">
+	<h3 class="head-section">The story</h3>
+	<p>
+		As a <a href="https://neovim.org" target="_blank">NeoVim</a> users, I have been "asked" why on earth
+		I use NeoVim for my daily driver for building products and doing some general programming. I've given
+		a lot of various answers over time, start from boasting about the performance benefits, the flex
+		of not touch my mouse, and so on.
+	</p>
+	<br />
+	<p>
+		But over the time, as I grown my understanding, as I widen my view of this world, I have found
+		few key reasons that I might think are why I keep sticking on using NeoVim.
+	</p>
+	<br />
+	<p>
+		I'd like to make this journals as my go to answer if in the future some peoples are asking me
+		why I do the way I do. Not because I don't want to answering them. But, I'd like to give them
+		the most thorough, clear, and concise answers possible.
+	</p>
+
+	<h3 class="head-section">I "own" it</h3>
+	<p>
+		I my self am not the creator of Vim or NeoVim, I am not that old (sorry, Bram✌️). But, since
+		NeoVim itself, after the installation, will only gives you the bare minimum things (like really
+		minimum), I have to add some plugins, configure it, and manage it to be able to use it as I want
+		to be.
+	</p>
+	<br />
+	<p>I think the last sentences are speaks a lot: <i>... as I want it to be</i>.</p>
+	<br />
+	<p>
+		Building, configuring, and managing my tools, that as a Software Engineer use in your day-to-day
+		life makes me always feels excited everytime I open my terminal and spins up my NeoVim. It
+		always prompt me to ask my self: <i>"Ah, what should I code today?"</i>.
+	</p>
+	<br />
+	<p>
+		My setups are so personalised for me that's why it is called as a "Personalised Development
+		Environment".
+	</p>
+
+	<h3 class="head-section">It prompts me to learn more</h3>
+	<p>
+		I've never heard about <a
+			href="https://en.wikipedia.org/wiki/Language_Server_Protocol"
+			target="_blank">LSP</a
+		> before, but I have to configure the LSP on my own editor.
+	</p>
+	<br />
+	<p>
+		I've never heard about <a
+			href="https://en.wikipedia.org/wiki/Abstract_syntax_tree"
+			target="_blank">AST</a
+		>, <a href="https://en.wikipedia.org/wiki/Parsing" target="_blank">parser</a> or any other cool computer
+		science terms before. But I have to be at least have some grasps on them in order to understand how
+		my editor works.
+	</p>
+
+	<h3 class="head-section">it's fun</h3>
+	<p>
+		i personally realised that building your pde is requires time and energy. i can gives you all of
+		the technical advantages and try to convince you to join the bandwagon. but, if you ask me to
+		why i keep doing this, is because it is fun for me.
+	</p>
+	<br />
+	<p>
+		I can spend my night just to choose the best colorscheme that suits my need and liking. <i
+			>It was fun.</i
+		>
+	</p>
+	<br />
+	<p>
+		I can spend some time on figuring out the best key-bindings for my frequent tasks. <i
+			>It was fun</i
+		>.
+	</p>
+	<h3 class="head-section">Should you try?</h3>
+	<p>
+		Yes! But, in the end I realised that the meost important thing is that you enjoy what you do. If
+		you think you much more comfortable working in Visual Studio Code, in IntelliJ, or anything else
+		(even Notepad), go for it.
+	</p>
+</div>
