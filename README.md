24c4a7e1fb339f10a18171158e68b15731e94bc2
Hisam Fahri <hisamafahri@Hisams-MacBook-Pro.local>
2023-09-28 12:48:53 +0000

    docs: adjust formatter

--- README.md
+++ README.md
@@ -37,4 +37,3 @@ To learn more about Svelte, take a look at the following resources:
 ## Author
 
 [@hisamafahri](https://github.com/hisamafahri)
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
 		extend: {
 			fontFamily: {
 				inter: ['Inter', 'sans-serif']
 			}
 		}
 	},
-  plugins: [],
-}
+	plugins: []
+};
